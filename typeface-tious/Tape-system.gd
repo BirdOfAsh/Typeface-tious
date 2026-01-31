540f939d1e2bd6d66b0b3a45159b3338d169b004
Aswath-sudhakar <amita003@d.umn.edu>
2026-01-31 16:24:14 -0600

    YAAAAAAAAAAAAY

--- typeface-tious/Tape-system.gd
+++ typeface-tious/Tape-system.gd
@@ -9,11 +9,12 @@ const Pwhizz = preload("uid://7xp3jlwl20t3")
 const Pwhizz2 = preload("uid://bt4py1a77veni")
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var button: Button = $Button
-@onready var face: Node2D = $"."
-@onready var face_box: Area2D = $FaceBox
+@onready var tape_box: Area2D = $TapeBox
 var dragging := false 
-var tape: Node2D
+var tape:  Area2D
+var tape_over_target = false 
+@onready var face: Area2D = $Facebox
+
 
 
 
@@ -21,9 +22,9 @@ func _process(delta):
 	if tape:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			tape.global_position = get_global_mouse_position()
-		else:
-			tape.queue_free()
-			tape = null
+				
+		
+			
 	
 
 
@@ -31,7 +32,7 @@ func _on_button_pressed():
 	Heal()
 	
 
-var StateNum : int = 0
+var StateNum : int = 1
 
 
 func ChangeStates():
@@ -62,8 +63,27 @@ func _on_face_box_input_event(viewport: Node, event: InputEvent, shape_idx: int)
 				
 				tape.global_position = get_global_mouse_position()
 				dragging = true 
-			if not event.pressed:
-				tape.queue_free()
-					
-				pass
+			# Connect signals to detect when tape enters/leaves target
+			
+		else:
+			# Release
+			dragging = false
+			if tape_over_target:
+				_on_tape_dropped_on_target()
+			tape.queue_free()
+			tape = null
+			tape_over_target = false
+	
+
+
+func _on_tape_dropped_on_target():
+	print("Tape dropped on target!")
 				
+
+
+func _on_facebox_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_facebox_area_exited(area: Area2D) -> void:
+	pass # Replace with function body.
