5622c4f8ccf35c13d719fd022abdbcadafb2a9fb
Maximusbat <braidenkovell@gmail.com>
2026-01-31 10:11:00 -0600

    removed unecessary code and restructued

--- typeface-tious/annoyanceAppear.gd
+++ typeface-tious/annoyanceAppear.gd
@@ -1,29 +1,14 @@
 extends Node2D
 
-var coordinatesOffScreen: Vector2 = position
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	coordinatesOffScreen = position
 
-var minTime : int = 40
-var maxTime : int = 60
-var elapsedTime : float
-var elapsedTimeInSecs : int
 var speed : float = 40.0
 var onScreen : bool = true
+var coordinatesOffScreen: Vector2 = position
 var coordinatesOnScreen : Vector2 = Vector2(100, 250)
-var lastValue : int
-
-func _process(delta: float) -> void:
-	elapsedTime += delta 
-	elapsedTimeInSecs = int(elapsedTime) #this might be weird with rounding, but it works so here we are,
-	#probably should also be deleted and moved to a game manager
 
 func _physics_process(_delta: float) -> void:
-	if elapsedTimeInSecs % 5 == 0 and elapsedTimeInSecs != lastValue:
-		lastValue = elapsedTimeInSecs
-		onScreen = !onScreen
-
 	if onScreen == true:
 		position = position.move_toward(coordinatesOnScreen, speed)
 	else:
@@ -33,3 +18,17 @@ func _physics_process(_delta: float) -> void:
 		hide()
 	else:
 		show()
+
+
+
+#this entire part should be removed once there is an actual trigger
+var elapsedTime : float
+var elapsedTimeInSecs : int
+var lastValue : int
+func _process(delta: float) -> void:
+	elapsedTime += delta 
+	elapsedTimeInSecs = int(elapsedTime) #this might be weird with rounding, but it works so here we are
+	
+	if elapsedTimeInSecs % 5 == 0 and elapsedTimeInSecs != lastValue:
+		lastValue = elapsedTimeInSecs
+		onScreen = !onScreen
