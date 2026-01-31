8d972480afbe0330bfd9ace302548468e34ce36b
Maximusbat <braidenkovell@gmail.com>
2026-01-31 15:12:57 -0600

    textBubble added
    spawns a text bubble when fully loaded in

---
+++ typeface-tious/ScreenBlockManager.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	firstObject = $GodotFace1
+	secondObject = $GodotFace2
+	thirdObject = $GodotFace3
+	forthObject = $GodotFace4
+	lastSpawnValue = 0
+	lastDespawnValue = 0
+
+
+var lastSpawnValue : int
+var lastDespawnValue : int
+
+var elapsedTime : float
+var elapsedTimeInSecs : int
+
+var firstObject : Node2D
+var secondObject : Node2D
+var thirdObject : Node2D
+var forthObject : Node2D 
+var objectsOnScreen : Array[int] = []
+
+var spawnRate : int = 10 #spawns something every given seconds
+var despawnRate : int = 5 #despawns oldest item every given seconds
+
+func _process(delta: float) -> void:
+	elapsedTime += delta 
+	elapsedTimeInSecs = int(elapsedTime) #this might be weird with rounding, and not completely accurate, but it works so here we are
+	
+	if elapsedTimeInSecs % despawnRate == 0 and elapsedTimeInSecs != lastDespawnValue:
+		print(elapsedTimeInSecs, " secs")
+		lastDespawnValue = elapsedTimeInSecs
+		if objectsOnScreen.size() > 0:
+			objectsOnScreen.erase(objectsOnScreen[0])
+	
+	if elapsedTimeInSecs % spawnRate == 0 and elapsedTimeInSecs != lastSpawnValue:
+		print(elapsedTimeInSecs, " secs")
+		lastSpawnValue = elapsedTimeInSecs
+		var randomChoice : int = (randi() % 4) + 1
+		objectsOnScreen.append(randomChoice)
+	
+	checkArray()
+
+#there is no way this is optimal programming but I can't think of anything better at the moment
+func checkArray() -> void:
+	firstObject.onScreen = true if objectsOnScreen.has(1) == true else false
+	secondObject.onScreen = true if objectsOnScreen.has(2) == true else false
+	thirdObject.onScreen = true if objectsOnScreen.has(3) == true else false
+	forthObject.onScreen = true if objectsOnScreen.has(4) == true else false
+	if objectsOnScreen.has(0) == true:
+		print("Something isn't indexed correctly")
---
+++ typeface-tious/onScreenBlockSpawn.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+func _ready() -> void:
+	marker = $Marker2D
+	sprite = $Sprite2D
+	textBubble = $SpriteTextBubble
+	coordinatesOnScreen = marker.position
+	coordinatesOffScreen = sprite.position
+
+var speed : float = 5.0
+var onScreen : bool = false
+var atLocation : bool = false #different from onScreen variable because onScreen is true while the sprite is moving
+var coordinatesOffScreen: Vector2
+var coordinatesOnScreen : Vector2
+var marker : Marker2D
+var sprite : Sprite2D
+var textBubble : Sprite2D
+
+
+func _physics_process(_delta: float) -> void:
+	if onScreen == true:
+		sprite.position = sprite.position.move_toward(coordinatesOnScreen, speed)
+	else:
+		sprite.position = sprite.position.move_toward(coordinatesOffScreen, speed)
+	
+	
+	if sprite.position == coordinatesOnScreen:
+		atLocation = true
+	elif sprite.position == coordinatesOffScreen:
+		#at starting location offscreen
+		hide()
+	else:
+		atLocation = false
+		show()
+	
+	
+	if atLocation == true:
+		textBubble.show()
+	else:
+		textBubble.hide()
