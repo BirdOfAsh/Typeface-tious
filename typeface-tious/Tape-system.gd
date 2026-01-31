0f5aeb06961a5d5aba333e6e3aed947caf6c1073
Aswath-sudhakar <amita003@d.umn.edu>
2026-01-31 11:27:07 -0600

    state functions setup

--- typeface-tious/Tape-system.gd
+++ typeface-tious/Tape-system.gd
@@ -2,8 +2,17 @@ extends Node2D
 signal Dead
 
 @onready var face: Node2D = $"."
-
+var StateNum : int = 0
 
 func ChangeStates():
+	if StateNum !=0 and StateNum > 4:
+		
+		pass
+
 	
-	pass
+func Damage():
+	StateNum = StateNum - 1
+	
+func Heal():
+	StateNum = StateNum + 1 
+	
