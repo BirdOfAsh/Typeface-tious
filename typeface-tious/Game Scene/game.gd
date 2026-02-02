fb83f8b8a3bd6135410839c0a0e74b306635bcdd
Aswath-sudhakar <amita003@d.umn.edu>
2026-02-01 19:50:16 -0600

    yaaaaaaaay

--- typeface-tious/Game Scene/game.gd
+++ typeface-tious/Game Scene/game.gd
@@ -9,9 +9,7 @@ func _ready() -> void:
 
 @onready var typing_system : TypingSystem = %"Typing System"
 
-func _ready() -> void:
-	#typing_system.day_finished.connect(day_won)
-	clock.clock_finished.connect(day_won)
+
 
 func day_won() -> void:
 	SignalBus.switch_scene.emit(self)
